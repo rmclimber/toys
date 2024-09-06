e2b47596147e7659811c4c5d4538e889d6e23e1c
RM <rmorris.code@gmail.com>
2024-09-06 15:44:55 -0700

    history added, now trying to get the board built with a loop

--- react/tictactoe/src/App.js
+++ react/tictactoe/src/App.js
@@ -6,9 +6,28 @@ function Square({ value , onSquareClick}) {
     <button className="square" onClick={onSquareClick}>{value}</button>);
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function MakeCell({ row, col, squares }) {
+  const i = row * 3 + col;
+  return (
+    <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+  );
+}
+
+function MakeRow({ row, squares }) {
+  return (
+    <div className="board-row">
+      { [0, 1, 2].map((col) => MakeCell(row, col, squares)) }
+    </div>
+  );
+}
+
+function MakeBoard({ squares }) {
+  return (
+    [0, 1, 2].map((row) => MakeRow(row, squares))
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -20,8 +39,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -35,7 +53,8 @@ export default function Board() {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
+      {MakeBoard(squares)}
+      {/* <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
@@ -49,7 +68,7 @@ export default function Board() {
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      </div> */}
     </>
   );
 }
@@ -72,4 +91,46 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 == 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move # ' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)} > {description} </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
 }
